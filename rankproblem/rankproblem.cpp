b080b1d6d8d416e41354077192792d9b7b10b970
Woo Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2022-08-17 10:57:36 +0800

    "A Rank Problem", Difficulty: 2.3, comitted via Kattis Tracker

---
+++ rankproblem/rankproblem.cpp
@@ -0,0 +1,38 @@
+// ***A Rank Problem Solution***
+// Difficulty: 2.3
+// Time Limit: 1 second, Memory Limit: 1024 MB
+// Acceptance: 56%
+// CPU Time: 0.00 s
+// Author: No author
+// Source: No source
+// Link: https://open.kattis.com/problems/rankproblem
+
+
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+int main() {
+    long n, m; cin >> n >> m;
+    vector<int> order(n);
+    iota(order.begin(), order.end(), 0);
+
+    while (m--) {
+        char dummy1, dummy2; int Ti, Tj; cin >> dummy1 >> Ti >> dummy2 >> Tj;
+        Ti--; Tj--;
+        int idx_i = find(order.begin(), order.end(), Ti) - order.begin();
+        int idx_j = find(order.begin(), order.end(), Tj) - order.begin();
+
+        if (idx_i > idx_j) { // a 'weaker' team beats a 'stronger' team
+            order.erase(find(order.begin(), order.end(), Tj));
+            order.insert(order.begin() + idx_i, Tj);
+        }
+    }
+
+    for (auto& Ti : order)
+        cout << 'T' << (Ti+1) << ' '; // go to 1-based
+    cout << '\n';
+    return 0;
+}
