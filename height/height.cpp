4d58447ddae0ee1054fc7f123c1b52304f8dca40
Woo Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2022-08-17 22:42:25 +0800

    "Height Ordering", Difficulty: 2.2, comitted via Kattis Tracker

---
+++ height/height.cpp
@@ -0,0 +1,38 @@
+// ***Height Ordering Solution***
+// Difficulty: 2.2
+// Time Limit: 1 second, Memory Limit: 1024 MB
+// Acceptance: 57%
+// CPU Time: 0.01 s
+// Author: No author
+// Source: 2014 Greater New York Region ACM Regional Contest
+// Link: https://open.kattis.com/problems/height
+
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long calculateSteps(vector<long> &people) {
+    long counter = 0;
+    for (long i = 1; i < people.size(); ++i) {
+        long temp = people[i];
+        long j = i - 1;
+        while (j >= 0 && people[j] > temp) {
+            swap(people[j], people[j + 1]);
+            counter++;
+            j--;
+        }
+    }
+    return counter;
+}
+
+int main() {
+    long x; cin >> x;
+    for (long i = 0; i < x; ++i) {
+        long a; cin >> a;
+        vector<long> people(20);
+        for (long j = 0; j < 20; ++j) cin >> people[j];
+        cout << a << " " << calculateSteps(people) << "\n";
+    }
+}
