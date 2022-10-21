9a440d663a2e93155470225799ee0aa0b490243d
Woo Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2022-10-21 19:34:08 +0800

    "Shattered Cake", Difficulty: 1.4, comitted via Kattis Tracker

--- shatteredcake/shatteredcake.cpp
+++ shatteredcake/shatteredcake.cpp
@@ -2,7 +2,7 @@
 // Difficulty: 1.4
 // Time Limit: 6 seconds, Memory Limit: 1024 MB
 // Acceptance: 64%
-// CPU Time: 1.04 s
+// CPU Time: 0.40 s
 // Author: Silviu Maniu
 // Source: Southwestern Europe Regional Contest (SWERC) 2017
 // Link: https://open.kattis.com/problems/shatteredcake
@@ -12,6 +12,7 @@
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     long long width, num, area = 0; cin >> width >> num;
     for (long long i = 0; i < num; ++i) {
         long a, b; cin >> a >> b;
