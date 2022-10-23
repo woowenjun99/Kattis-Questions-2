1cc4c6a9ec78e272ea5206b07f9b746c37c07b04
Woo Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2022-10-23 14:24:29 +0800

    "Single source shortest path, safe path", Difficulty: 4.9, comitted via Kattis Tracker

---
+++ shortestpath4/shortestpath4.cpp
@@ -0,0 +1,71 @@
+// ***Single source shortest path, safe path Solution***
+// Difficulty: 4.9
+// Time Limit: 1 second, Memory Limit: 1024 MB
+// Acceptance: 10%
+// CPU Time: 0.21 s
+// Author: Steven Halim
+// Source: NUS Competitive Programming
+// Link: https://open.kattis.com/problems/shortestpath4
+
+
+#define INF 1e9
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int Query(int s, int t, int k, vector<vii> AL) {
+    // Create a distance array like Bellman-Ford Algorithm
+    vector<int> dist(AL.size(), INF);
+    dist[s] = 0;
+
+    // Go through k layers 
+    for (int i = 0; i < k - 1; ++i) {
+        vector<int> cloned = dist;
+        for (int j = 0; j < AL.size(); ++j) {
+            for (int k = 0; k < AL[j].size(); ++k) {
+                int neighbour = AL[j][k].first;
+                int time = AL[j][k].second;
+
+                if (dist[j] == INF) continue;
+                if (dist[j] + time < cloned[neighbour]) {
+                    cloned[neighbour] = dist[j] + time;
+                }
+            }
+        }
+        dist = cloned;
+    }
+
+    if (dist[t] == INF) return -1;
+    return dist[t];
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int TC; cin >> TC;
+    while (TC--) {
+        int V; cin >> V;
+        vector<vii> AL(V, vii());
+
+        // Creating a adjacency list
+        for (int i = 0; i < V; ++i) {
+            int X; cin >> X;
+            while (X--) {
+                int a, b; cin >> a >> b;
+                AL[i].emplace_back(a, b);
+            }
+        }
+
+        int Q; cin >> Q;
+        while (Q--) {
+            int s, t, k; cin >> s >> t >> k;
+            cout << Query(s, t, k, AL) << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
